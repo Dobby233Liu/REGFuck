0771813df4b0d93be48fd95334993273df90a1cb
Liu-Wen-Yuan <15816141883@163.com>
2017-06-11 04:23:45 -0500

    Create Program.cs

--- RegFuck/Program.cs
+++ RegFuck/Program.cs
@@ -14,10 +14,10 @@ namespace RegFuck
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            if (MessageBox.Show("This program was made to destroy the registry of your computer.\n" + 
-                "Using it will likely make your computer unbootable or unusable.\n" +
-                "The GUI of this program was made intentionally bad, if you don't know what you do, don't continue.\n\n" +
-                "You have been warned. Clicking Yes will start the program now.", "REGFuck - Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+         //   if (MessageBox.Show("This program was made to destroy the registry of your computer.\n" + 
+        //        "Using it will likely make your computer unbootable or unusable.\n" +
+       //         "The GUI of this program was made intentionally bad, if you don't know what you do, don't continue.\n\n" +
+    //            "You have been warned. Clicking Yes will start the program now.", "REGFuck - Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
                 Application.Run(new Form1());
         }
     }
